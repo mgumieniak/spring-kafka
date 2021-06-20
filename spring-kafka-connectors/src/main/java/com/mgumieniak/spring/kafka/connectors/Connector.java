781f2b036ee221463f8c23c6055e53c1dfc54e75
mgumieniak <maciejgumieniak007@gmail.com>
2021-06-20 12:12:28 +0200

    Init commit

---
+++ spring-kafka-connectors/src/main/java/com/mgumieniak/spring/kafka/connectors/Connector.java
@@ -0,0 +1,7 @@
+package com.mgumieniak.spring.kafka.connectors;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class Connector {
+}
---
+++ spring-kafka-models/src/main/java/com/mgumieniak/spring/kafka/models/Test.java
@@ -0,0 +1,8 @@
+package com.mgumieniak.spring.kafka.models;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class Test {
+    String temp;
+}
